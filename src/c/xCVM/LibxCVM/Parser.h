a9b45e8c4e8d38f8493b72b7019b4b11eda07a99
Creeper Lv <creeperlv@users.noreply.github.com>
2023-03-31 23:43:46 +0800

    Working on c version of parser.

---
+++ src/c/xCVM/LibxCVM/Parser.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "base.h"
+
+typedef struct LineComId {
+	char* startseq;
+}LineCommentIdentifier;
+
+typedef struct ClosableComId {
+	GenericList Start;
+	GenericList End;
+}ClosableCommentIdentifier;
+
+typedef struct SegEncId {
+	char L;
+	char R;
+} SegmentEncapsulationIdentifier;
+
+typedef struct parserconf {
+	GenericList PredefinedSegmentTemplate;
+	GenericList PredefinedSegmentCharacters;
+	GenericList SegmentEncapsulationIdentifiers;
+	GenericList lineCommentIdentifiers;
+	/////////////////////////////
+	//ClosableCommentIdentifier//
+	/////////////////////////////
+	GenericList closableCommentIdentifiers;
+}ParserConfiguration;
