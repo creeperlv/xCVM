9e6a130ba965955aec319377a0aebeb49362386a
Creeper Lv <creeperlv@users.noreply.github.com>
2023-06-20 00:18:26 +0800

    Working on expression parser.

--- src/dotNet/Cx/Cx.Core/VCParser/ExpressionParser.cs
+++ src/dotNet/Cx/Cx.Core/VCParser/ExpressionParser.cs
@@ -1,4 +1,5 @@
 ﻿using LibCLCC.NET.TextProcessing;
+using System.Collections.Generic;
 using xCVM.Core.CompilerServices;
 
 namespace Cx.Core.VCParser
@@ -9,6 +10,28 @@ namespace Cx.Core.VCParser
         public ExpressionSegment? Next;
         public Segment? Segment;
         public TreeNode? Node;
+        public ExpressionSegmentContext? Context;
+    }
+    public class ExpressionSegmentContext
+    {
+        public ExpressionSegment HEAD;
+        public ExpressionSegment? this [ int index ]
+        {
+            get
+            {
+                ExpressionSegment? current = HEAD;
+                for (int i = 0 ; i <= index ; i++)
+                {
+                    if (current == null) return null;
+                    current = current.Next;
+                }
+                return current;
+            }
+        }
+        public ExpressionSegmentContext(ExpressionSegment head)
+        {
+            HEAD = head;
+        }
     }
     public class ExpressionParser : ContextualParser
     {
@@ -17,22 +40,30 @@ namespace Cx.Core.VCParser
             OperationResult<bool> FinalResult = false;
             return FinalResult;
         }
-    }
-    public class BinaryExpressionParser : ContextualParser
-    {
-        public override OperationResult<bool> Parse(ParserProvider provider , SegmentContext context , TreeNode Parent)
+        public SegmentContext TerminateExpression(SegmentContext context) {
+            var HEAD = context.Current;
+            var ENDPOINT = context.EndPoint;
+            while (true)
+            {
+
+            }
+            SegmentContext Result=new SegmentContext(HEAD);
+            Result.SetEndPoint(ENDPOINT);
+            
+        }
+        public OperationResult<ExpressionSegment?> FirstPass_SubstituteCalls(SegmentContext context)
         {
-            OperationResult<bool> FinalResult = false;
-            context.Mat(true , "*" , "/");
+            OperationResult<ExpressionSegment?> FinalResult = new OperationResult<ExpressionSegment?>(null);
+
             return FinalResult;
         }
     }
-    public class UnaryExpressionParser : ContextualParser
+    public static class ExpressionSymbols
     {
-        public override OperationResult<bool> Parse(ParserProvider provider , SegmentContext context , TreeNode Parent)
-        {
-            OperationResult<bool> FinalResult = false;
-            return FinalResult;
-        }
+        public static readonly string [ ] Termination = new string [ ] { "," , ")" , ";" };
+        public static readonly string [ ] Binary_0st = new string [ ] { "&&" , "||" , "&" , "|" };
+        public static readonly string [ ] Binary_1st = new string [ ] { "*" , "/" , "%" };
+        public static readonly string [ ] Binary_2st = new string [ ] { "+" , "-" };
+
     }
 }
