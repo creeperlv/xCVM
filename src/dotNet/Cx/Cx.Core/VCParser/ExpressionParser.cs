ec191eab3eada43dc3856bc1b0e20985b556aac7
Creeper Lv <creeperlv@users.noreply.github.com>
2023-06-22 02:51:34 +0800

    Working on the expression parser.

--- src/dotNet/Cx/Cx.Core/VCParser/ExpressionParser.cs
+++ src/dotNet/Cx/Cx.Core/VCParser/ExpressionParser.cs
@@ -11,46 +11,11 @@ namespace Cx.Core.VCParser
         public Segment? Segment;
         public TreeNode? Node;
         public ExpressionSegmentContext? Context;
+        public int ID;
         public void AttachNext(ExpressionSegment next)
         {
             Next = next;
             next.Prev = this;
-        }
-    }
-    public class ExpressionSegmentContext
-    {
-        public ExpressionSegment HEAD;
-        public int Count
-        {
-            get
-            {
-                ExpressionSegment Current = HEAD;
-                int Count = 1;
-                while (true)
-                {
-                    if (Current.Next == null)
-                    {
-                        return Count;
-                    }
-                }
-            }
-        }
-        public ExpressionSegment? this [ int index ]
-        {
-            get
-            {
-                ExpressionSegment? current = HEAD;
-                for (int i = 0 ; i <= index ; i++)
-                {
-                    if (current == null) return null;
-                    current = current.Next;
-                }
-                return current;
-            }
-        }
-        public ExpressionSegmentContext(ExpressionSegment head)
-        {
-            HEAD = head;
         }
     }
     public class ExpressionParser : ContextualParser
@@ -62,10 +27,26 @@ namespace Cx.Core.VCParser
                 var TER = TerminateExpression(provider , context);
                 if (FinalResult.CheckAndInheritAbnormalities(TER)) return FinalResult;
                 var _newContext = TER.Result;
+
                 var FPR = FirstPass_SubstituteCalls(provider , _newContext);
                 if (FinalResult.CheckAndInheritAbnormalities(FPR)) return FinalResult;
-
+                if (FPR.Result == null)
+                {
+                    FinalResult.AddError(new ParseFailError(_newContext.HEAD , ASTNodeType.Expression));
+                    return FinalResult;
+                }
+                ExpressionSegmentContext ES_Context = new ExpressionSegmentContext(FPR.Result);
+                ParseExpressionTree(provider , ES_Context);
             }
+            return FinalResult;
+        }
+        public OperationResult<TreeNode> ParseExpressionTree(ParserProvider provider , ExpressionSegmentContext context)
+        {
+            TreeNode Node = new TreeNode();
+            Node.Type = ASTNodeType.Expression;
+            OperationResult<TreeNode> FinalResult = new OperationResult<TreeNode>(Node);
+            var SP_C_R=SecondPass_Closures(provider , context);
+            if(FinalResult.CheckAndInheritAbnormalities(SP_C_R))return FinalResult;
             return FinalResult;
         }
         public OperationResult<SegmentContext> TerminateExpression(ParserProvider provider , SegmentContext context)
@@ -105,6 +86,56 @@ namespace Cx.Core.VCParser
             Result.SetEndPoint(ENDPOINT);
             return Result;
         }
+        public OperationResult<bool> CustomPass_BinaryExpression(ParserProvider provider , SegmentContext context , params string [ ] symbols)
+        {
+            OperationResult<bool> FinalResult = new OperationResult<bool>(false);
+            return FinalResult;
+        }
+        public OperationResult<bool> SecondPass_Closures(ParserProvider provider , ExpressionSegmentContext context)
+        {
+            OperationResult<bool> FinalResult = new OperationResult<bool>(false);
+            ExpressionSegment? FirstParentheses_L = null;
+            int ParenthesesDepth = 0;
+            while (true)
+            {
+                if (context.IsReachEnd) break;
+                if (context.Match("(") == ESCMatchResult.Match)
+                {
+                    if (FirstParentheses_L == null)
+                    {
+                        FirstParentheses_L = context.Current;
+                    }
+                    ParenthesesDepth++;
+                }
+                if (context.Match(")") == ESCMatchResult.Match)
+                {
+                    if (FirstParentheses_L == null)
+                    {
+                        FinalResult.AddError(new ClosureError(context.Current?.Segment , "(" , ")"));
+                        return FinalResult;
+                    }
+
+                    ParenthesesDepth--;
+                    if (ParenthesesDepth == 0)
+                    {
+                        ExpressionSegment Closure = new ExpressionSegment();
+                        ExpressionSegmentContext Closed = new ExpressionSegmentContext(FirstParentheses_L.Next!);
+                        Closure.Context = Closed;
+                        var R = context.Current;
+                        context.GoNext();
+                        context.SubstituteSegment_0(FirstParentheses_L?.Prev , R?.Next , Closure);
+                        //Recursive Parse.
+                        var CResult = ParseExpressionTree(provider , Closed);
+                        if (FinalResult.CheckAndInheritAbnormalities(CResult)) return FinalResult;
+                        var nt = CResult.Result;
+                        Closure.Node = nt;
+                        FirstParentheses_L = null;
+                    }
+                }
+            }
+            FinalResult.Result = true;
+            return FinalResult;
+        }
         public OperationResult<ExpressionSegment?> FirstPass_SubstituteCalls(ParserProvider provider , SegmentContext context)
         {
             OperationResult<ExpressionSegment?> FinalResult = new OperationResult<ExpressionSegment?>(null);
@@ -119,16 +150,18 @@ namespace Cx.Core.VCParser
             TreeNode Reciver = new TreeNode();
             ExpressionSegment? ES_HEAD = null;
             ExpressionSegment? Current = null;
+            int ID = 0;
             while (true)
             {
                 if (context.ReachEnd) break;
                 var CPR = CallParser.Parse(provider , context , Reciver);
                 if (FinalResult.CheckAndInheritAbnormalities(CPR)) { return FinalResult; }
+                ExpressionSegment new_segment = new ExpressionSegment();
+                new_segment.ID = ID;
                 if (CPR.Result)
                 {
                     var NewNode = Reciver.Children [ 0 ];
                     Reciver.Children.Clear();
-                    ExpressionSegment new_segment = new ExpressionSegment();
                     new_segment.Node = NewNode;
                     if (ES_HEAD == null || Current == null)
                     {
@@ -143,7 +176,6 @@ namespace Cx.Core.VCParser
                 }
                 else
                 {
-                    ExpressionSegment new_segment = new ExpressionSegment();
                     new_segment.Segment = context.Current;
                     if (ES_HEAD == null || Current == null)
                     {
@@ -157,6 +189,7 @@ namespace Cx.Core.VCParser
                     }
                     context.GoNext();
                 }
+                ID++;
             }
             return ES_HEAD;
         }
---
+++ src/dotNet/Cx/Cx.Core/VCParser/ExpressionSegmentContext.cs
@@ -0,0 +1,149 @@
+﻿namespace Cx.Core.VCParser
+{
+    public enum ExpressionSegmentType
+    {
+        PlainContent, TreeNode, Closure, NULL
+    }
+    public enum ESCMatchResult
+    {
+        Match, MismatchContent, MismatchType, ReachEnd
+    }
+    public class ExpressionSegmentContext
+    {
+        public ExpressionSegment HEAD;
+        private ExpressionSegment? _Current;
+        public ExpressionSegment? Current { get => _Current; }
+        private ExpressionSegment? _ep = null;
+        public ExpressionSegment? EndPoint { get => _ep; }
+        public ExpressionSegment? Next;
+        public ExpressionSegment? Prev;
+        public void GoNext()
+        {
+            Prev = Current;
+            _Current = _Current?.Next;
+            Next = Current?.Next;
+        }
+        public void GoBack()
+        {
+            _Current = Prev;
+            Prev = Current?.Prev;
+            Next = Current?.Next;
+        }
+        public ExpressionSegmentType CurrentType
+        {
+            get
+            {
+                if (Current?.Segment != null) return ExpressionSegmentType.PlainContent;
+                if (Current?.Node != null) return ExpressionSegmentType.TreeNode;
+                if (Current?.Context != null) return ExpressionSegmentType.Closure;
+                return ExpressionSegmentType.NULL;
+            }
+        }
+        public ESCMatchResult Match(string content)
+        {
+            switch (CurrentType)
+            {
+                case ExpressionSegmentType.PlainContent:
+                    if (Current?.Segment?.content == content) return ESCMatchResult.Match;
+                    return ESCMatchResult.MismatchContent;
+                case ExpressionSegmentType.TreeNode:
+                case ExpressionSegmentType.Closure:
+                    return ESCMatchResult.MismatchType;
+                default:
+                    return ESCMatchResult.ReachEnd;
+
+            }
+        }
+        public ESCMatchResult MatchCollection(params string [ ] contents)
+        {
+            switch (CurrentType)
+            {
+                case ExpressionSegmentType.PlainContent:
+                    foreach (var content in contents)
+                    {
+                        if (Current?.Segment?.content == content) return ESCMatchResult.Match;
+                    }
+                    return ESCMatchResult.MismatchContent;
+                case ExpressionSegmentType.TreeNode:
+                case ExpressionSegmentType.Closure:
+                    return ESCMatchResult.MismatchType;
+                default:
+                    return ESCMatchResult.ReachEnd;
+
+            }
+        }
+        public void SetCurrent(ExpressionSegment? current)
+        {
+            _Current = current;
+        }
+        public void SetEndPoint(ExpressionSegment expressionSegment)
+        {
+            _ep = expressionSegment;
+        }
+        public bool IsReachEnd { get { return Current == EndPoint || Current?.Next == null; } }
+        public bool SubstituteSegment_1(ExpressionSegment? L,ExpressionSegment? R)
+        {
+            if (L == null && R == null) return false;
+            if (L?.Next == R) return false;
+            if (L == null)
+            {
+                if (R == null) return false;
+                if (R.Next == null) return false;
+                HEAD = R.Next;
+                return true;
+            }
+            if (L.Prev == null)
+            {
+                if (R == null)
+                {
+                    return false;
+                }
+                if (R.Next == null) return false;
+                HEAD = R.Next;
+                return true;
+            }
+            if (R == null)
+            {
+                L.Prev.Next = null;
+                return true;
+            }
+            if(R.Next==null)
+            {
+                L.Prev.Next = null;
+                return true;
+            }
+            L.Prev.Next = R.Next;
+            R.Next.Prev = L.Prev;
+            return true;
+        }
+        public bool SubstituteSegment_0(ExpressionSegment? LeftEnd , ExpressionSegment? RightEnd , ExpressionSegment NewSegment)
+        {
+            var segment_l_end = LeftEnd;
+            var segment_r_end = RightEnd;
+            if (segment_l_end == null)
+            {
+                this.HEAD = NewSegment;
+            }
+            else
+            {
+                segment_l_end.Next = NewSegment;
+                NewSegment.Prev = segment_l_end;
+            }
+            if (segment_r_end != null)
+            {
+                NewSegment.Next = segment_r_end;
+                segment_r_end.Prev = NewSegment;
+            }
+            else
+            {
+                SetEndPoint(NewSegment);
+            }
+            return true;
+        }
+        public ExpressionSegmentContext(ExpressionSegment head)
+        {
+            HEAD = head;
+            _Current = head;
+        }
+    }
+}
