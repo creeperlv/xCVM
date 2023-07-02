60001c49ddb385ad5ef3a58814143a4f74745f36
Creeper Lv <creeperlv@users.noreply.github.com>
2023-07-03 00:44:28 +0800

    Working on analyzers.

---
+++ src/dotNet/Cx/Cx.Core/Analyzer/CAnalyzer.cs
@@ -0,0 +1,67 @@
+﻿using LibCLCC.NET.TextProcessing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using xCVM.Core.CompilerServices;
+
+namespace Cx.Core.Analyzer
+{
+    public class CAnalyzer
+    {
+        public List<int> ConcernedAnalyzers=new List<int>();
+        public virtual OperationResult<(bool, Symbol?)> Analyze(AnalyzerProvider provider,int Pos,ref TreeNode node)
+        {
+            return (false,null);
+        }
+    }
+    public class AnalyzerProvider
+    {
+        Dictionary<int , CAnalyzer> analyzers = new Dictionary<int , CAnalyzer>();
+        public void RegisterAnalyzer(int ID , CAnalyzer parser)
+        {
+            if (analyzers.ContainsKey(ID))
+            {
+                analyzers [ ID ] = parser;
+            }
+            else analyzers.Add(ID , parser);
+        }
+        public CAnalyzer? GetAnalyzer(int ID)
+        {
+            if (analyzers.ContainsKey(ID))
+            {
+                return analyzers [ ID ];
+            }
+            return null;
+        }
+    }
+    public class AnalyzedTreeNode : TreeNode
+    {
+        public SymbolTable? table = null;
+    }
+    public enum SymbolType
+    {
+        Function, Variable, TypeDefinition, StructDeclaration
+    }
+    public class Symbol
+    {
+        public Segment Name;
+        public TreeNode ReferredNode;
+        public int Position;
+        public SymbolType symbolType;
+        public Symbol(Segment name , TreeNode referredNode , int position , SymbolType symbolType)
+        {
+            Name = name;
+            ReferredNode = referredNode;
+            Position = position;
+            this.symbolType = symbolType;
+        }
+    }
+    public class SymbolTable
+    {
+        List<Symbol> Symbols = new List<Symbol>();
+        public void Add(Symbol symbol)
+        {
+            Symbols.Add(symbol);
+        }
+    }
+}
--- src/dotNet/Cx/Cx.Core/TreeNode.cs
+++ src/dotNet/Cx/Cx.Core/TreeNode.cs
@@ -1,6 +1,7 @@
 ﻿using LibCLCC.NET.TextProcessing;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Cx.Core
 {
@@ -12,11 +13,20 @@ namespace Cx.Core
         [NonSerialized]
         public TreeNode? Parent;
         public List<TreeNode> Children = new List<TreeNode>();
+        public void ReplaceChild(TreeNode OriginalChild , TreeNode NewNode)
+        {
+            var ch = Children.IndexOf(OriginalChild);
+            NewNode.Parent = this;
+            foreach (var item in OriginalChild.Children)
+            {
+                NewNode.AddChild(item);
+            }
+            Children [ ch ] = NewNode;
+            OriginalChild.Children.Clear();
+            OriginalChild.Parent = null;
+        }
         public void AddChild(TreeNode node)
         {
-#if DEBUG
-            Console.WriteLine("Added node.");
-#endif
             Children.Add(node);
             node.Parent = this;
         }
