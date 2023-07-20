3dcc38195bca213297273476c6323fb57a368b7a
Creeper Lv <creeperlv@users.noreply.github.com>
2023-07-21 01:16:05 +0800

    CodeGenerator now should use CheckThenRun to filter nodes.

--- src/dotNet/Cx/Cx.Core/CodeGen/CodeGenerator.cs
+++ src/dotNet/Cx/Cx.Core/CodeGen/CodeGenerator.cs
@@ -7,56 +7,70 @@ using xCVM.Core.CompilerServices;
 
 namespace Cx.Core.CodeGen
 {
-    public class CodeGenerator
-    {
-        public virtual OperationResult<bool> Write(GeneratorProvider provider, AnalyzedTreeNode node , StreamWriter writer)
-        {
-            return new OperationResult<bool>(false);
-        }
-    }
-    public class ToASM_Scope_Generator : CodeGenerator
-    {
-
-    }
-    public class ToASM_CallGenerator : CodeGenerator
-    {
-		public override OperationResult<bool> Write(GeneratorProvider provider , AnalyzedTreeNode node , StreamWriter writer)
+	public class CodeGenerator
+	{
+		public virtual OperationResult<bool> Write(GeneratorProvider provider , AnalyzedTreeNode node , StreamWriter writer)
 		{
-            if(node.Type!= ASTNodeType.Call)
-            {
-                switch (node.Segment?.content??"")
-                {
-                    case "ASM_0":
-                        {
-
-                        }
-                        break;
-                    default:
-                        break;
-                }
-            }
-            return false;
+			return new OperationResult<bool>(false);
+		}
+		public OperationResult<bool> CheckThenRun(AnalyzedTreeNode node , int [ ] AcceptedIDs , Func<OperationResult<bool>> func)
+		{
+			bool Hit = false;
+			foreach (var item in AcceptedIDs)
+			{
+				if (node.Type == item)
+				{
+					Hit = true;
+					break;
+				}
+			}
+			if (!Hit) { return false; }
+			return func();
 		}
 	}
-    public class GeneratorProvider
-    {
+	public class ToASM_Scope_Generator : CodeGenerator
+	{
+		static readonly int [ ] types = new int [ ] { ASTNodeType.Scope };
+		public override OperationResult<bool> Write(GeneratorProvider provider , AnalyzedTreeNode node , StreamWriter writer)
+		{
+			return CheckThenRun(node , types , () => { return false; });
+		}
+	}
+	public class ToASM_RawAssembly_Generator : CodeGenerator
+	{
+		static readonly int [ ] types = new int [ ] { ASTNodeType.RawAssembly };
+		public override OperationResult<bool> Write(GeneratorProvider provider , AnalyzedTreeNode node , StreamWriter writer)
+		{
+			return CheckThenRun(node , types , () => { return false; });
+		}
+	}
+	public class ToASM_CallGenerator : CodeGenerator
+	{
+		static readonly int [ ] types = new int [ ] { ASTNodeType.Call };
+		public override OperationResult<bool> Write(GeneratorProvider provider , AnalyzedTreeNode node , StreamWriter writer)
+		{
+			return CheckThenRun(node , types , () => { return false; });
+		}
+	}
+	public class GeneratorProvider
+	{
 
-        internal Dictionary<int , CodeGenerator> generators = new Dictionary<int , CodeGenerator>();
-        public void RegisterGenerator(int ID , CodeGenerator generator)
-        {
-            if (generators.ContainsKey(ID))
-            {
-                generators [ ID ] = generator;
-            }
-            else generators.Add(ID , generator);
-        }
-        public CodeGenerator? GetGenerator(int ID)
-        {
-            if (generators.ContainsKey(ID))
-            {
-                return generators [ ID ];
-            }
-            return null;
-        }
-    }
+		internal Dictionary<int , CodeGenerator> generators = new Dictionary<int , CodeGenerator>();
+		public void RegisterGenerator(int ID , CodeGenerator generator)
+		{
+			if (generators.ContainsKey(ID))
+			{
+				generators [ ID ] = generator;
+			}
+			else generators.Add(ID , generator);
+		}
+		public CodeGenerator? GetGenerator(int ID)
+		{
+			if (generators.ContainsKey(ID))
+			{
+				return generators [ ID ];
+			}
+			return null;
+		}
+	}
 }
