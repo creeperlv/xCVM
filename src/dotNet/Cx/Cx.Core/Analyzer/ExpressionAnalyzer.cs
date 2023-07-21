3aa147fcc35e7d3432af4d850f654e2fbc39f196
Creeper Lv <creeperlv@users.noreply.github.com>
2023-07-22 00:49:22 +0800

    Working on ExpressionAnalyzer.

--- src/dotNet/Cx/Cx.Core/Analyzer/ExpressionAnalyzer.cs
+++ src/dotNet/Cx/Cx.Core/Analyzer/ExpressionAnalyzer.cs
@@ -14,6 +14,15 @@ namespace Cx.Core.Analyzer
 				ASTNodeType.UnaryExpression,
 			};
 		}
+		public override OperationResult<bool> ReferenceAnalyze(AnalyzerProvider provider , int Pos , AnalyzedTreeNode node)
+		{
+			OperationResult<bool> FinalResult = false;
+			if (node.Type != ASTNodeType.Expression)
+			{
+				return FinalResult;
+			}
+			return FinalResult;
+		}
 		public override OperationResult<bool> BuildSymbolTable(AnalyzerProvider provider , int Pos , SymbolTable ParentTable , ref TreeNode node)
 		{
 			OperationResult<bool> FinalResult = false;
@@ -21,7 +30,12 @@ namespace Cx.Core.Analyzer
 			{
 				return FinalResult;
 			}
-			CAnalyzerUtilities.SubAnalyze_BuildSymbolTable(ConcernedSymbolTableBuildingAnalyzers , provider , node , ParentTable);
+			var st=CAnalyzerUtilities.SubAnalyze_BuildSymbolTable(ConcernedSymbolTableBuildingAnalyzers , provider , node , ParentTable);
+			if (FinalResult.CheckAndInheritAbnormalities(st))
+			{
+				return FinalResult;
+			}
+			FinalResult.Result = true;
 			return FinalResult;
 		}
 	}
